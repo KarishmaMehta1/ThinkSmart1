9eac712328ab4ea559be70510496a553a7d530e9
Karishma Mehta <Karishma.19mehta@gmail.com>
2017-10-14 18:35:44 +0200

    added

---
+++ index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Think Smart | Blog</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" type="text/css" href="css/coin-slider.css" />
+<script type="text/javascript" src="js/cufon-yui.js"></script>
+<script type="text/javascript" src="js/cufon-georgia.js"></script>
+<script type="text/javascript" src="js/jquery-1.4.2.min.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
+<script type="text/javascript" src="js/coin-slider.min.js"></script>
+</head>
+<body>
+<div class="main">
+  <div class="header">
+    <div class="header_resize">
+      <div class="menu_nav">
+        <ul>
+          <li><a href="index.php"><span>Home Page</span></a></li>
+          <li><a href="#"><span>Forum</span></a></li>
+          <li><a href="#"><span>About Us</span></a></li>
+          <li><a href="#"><span>Contact Us</span></a></li>
+        </ul>
+      </div>
+      <div class="logo">
+        <h1><a href="index.php"><img src="../img/logo.png" height=80 width=200/></a></h1>
+      </div>
+      <div class="clr"></div>
+     
+        <div class="clr"></div>
+      </div>
+      <div class="clr"></div>
+    </div>
+  </div>
+  <div class="content">
+    <div class="content_resize">
+      <div class="mainbar">
+      
+      <?php
+	  	
+		require '../include/config.php';
+		$viewPosts = $link -> prepare("SELECT * FROM blog_posts ORDER BY datetime DESC;");
+		$viewPosts -> execute();
+		while($result = $viewPosts->fetch(PDO::FETCH_ASSOC))
+		{
+			?>
+            
+            <div class="article">
+	          <h2><a href="./viewPost.php?post_id=<?php print $result['post_id']; ?>"><?php print $result['post_title']; ?></a></h2>
+    	      <div class="clr"></div>
+        		  <p><?php print $result['post']; ?></p>
+		          <p>Tagged: <a href="#">
+                  <?php
+				  	$getcategory=$link->prepare("select * from category where cat_id='$result[cat_id]';");
+					$getcategory->execute();
+					$res=$getcategory->fetch(PDO::FETCH_ASSOC);
+					echo $res['cat_name'];
+				  ?>
+                  </a></p>
+          			<p><a href="#"><strong>Comments (<?php echo $result['comments'];?>)</strong></a> <span>&nbsp;&bull;&nbsp;</span> <?php print $result['datetime']; ?></p>
+        	</div>
+            
+            <?php
+		}
+		
+	  ?>
+      </div>
+      <div class="sidebar">
+        
+        <div class="gadget">
+          <h2 class="star"><span>Category</span></h2>
+          <div class="clr"></div>
+          <ul class="sb_menu">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">TemplateInfo</a></li>
+            <li><a href="#">Style Demo</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Archives</a></li>
+            <li><a href="#">Web Templates</a></li>
+          </ul>
+        </div>
+  
+      </div>
+      <div class="clr"></div>
+    </div>
+  </div>
+  <div class="fbg">
+    <div class="fbg_resize">
+      <div class="col c1">
+        <h2><span>Image</span> Gallery</h2>
+        <a href="#"><img src="images/gal1.jpg" width="75" height="75" alt="" class="gal" /></a> <a href="#"><img src="images/gal2.jpg" width="75" height="75" alt="" class="gal" /></a> <a href="#"><img src="images/gal3.jpg" width="75" height="75" alt="" class="gal" /></a> <a href="#"><img src="images/gal4.jpg" width="75" height="75" alt="" class="gal" /></a> <a href="#"><img src="images/gal5.jpg" width="75" height="75" alt="" class="gal" /></a> <a href="#"><img src="images/gal6.jpg" width="75" height="75" alt="" class="gal" /></a> </div>
+      <div class="col c2">
+        <h2><span>Services</span> Overview</h2>
+        <p>Curabitur sed urna id nunc pulvinar semper. Nunc sit amet tortor sit amet lacus sagittis posuere cursus vitae nunc.Etiam venenatis, turpis at eleifend porta, nisl nulla bibendum justo.</p>
+        <ul class="fbg_ul">
+          <li><a href="#">Lorem ipsum dolor labore et dolore.</a></li>
+          <li><a href="#">Excepteur officia deserunt.</a></li>
+          <li><a href="#">Integer tellus ipsum tempor sed.</a></li>
+        </ul>
+      </div>
+      <div class="col c3">
+        <h2><span>Contact</span> Us</h2>
+        <p>Nullam quam lorem, tristique non vestibulum nec, consectetur in risus. Aliquam a quam vel leo gravida gravida eu porttitor dui.</p>
+        <p class="contact_info"> <span>Address:</span> 1458 TemplateAccess, USA<br />
+          <span>Telephone:</span> +123-1234-5678<br />
+          <span>FAX:</span> +458-4578<br />
+          <span>Others:</span> +301 - 0125 - 01258<br />
+          <span>E-mail:</span> <a href="#">mail@yoursitename.com</a> </p>
+      </div>
+      <div class="clr"></div>
+    </div>
+  </div>
+  <div class="footer">
+    <div class="footer_resize">
+      <p class="lf">&copy; Copyright <a href="http://www.thinksmart.bugs3.com">Think Smart</a>.</p>
+      <p class="rf">Design by Dream <a href="http://www.dreamtemplate.com/">Web Templates</a></p>
+      <div style="clear:both;"></div>
+    </div>
+  </div>
+</div>
+<div align=center>Developed by <a href='http://www.thinksmart.bugs3.com'>Think Smart</a></div></body>
+</html>
